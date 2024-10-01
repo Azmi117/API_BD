518ebf7ef7bb0395f180d769b29b68c8fa570eac
azmiyushari <azmiyushrr@gmail.com>
2024-10-01 15:48:19 +0700

    Reverse

--- config/config.js
+++ config/config.js
@@ -1,22 +1,25 @@
-require('dotenv').config(); // Pastikan ini ada di paling atas
+require('dotenv').config();
 
 module.exports = {
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.HOST,
-    dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT
-  },
-  production: {
-    use_env_variable: 'DATABASE_URL', // Gunakan nama variabel yang sesuai
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  }
-};
+    "development": {
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host": process.env.HOST,
+        "dialect": process.env.DB_DIALECT,
+      },
+      "test": {
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host": process.env.HOST,
+        "dialect": process.env.DB_DIALECT
+      },
+      "production": {
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host": process.env.HOST,
+        "dialect": process.env.DB_DIALECT
+      },
+}
