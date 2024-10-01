a5fab70fe55106144a2d77f7a14e4f1300a6e45c
azmiyushari <azmiyushrr@gmail.com>
2024-10-01 15:37:46 +0700

    Fix:dotenv

--- config/config.js
+++ config/config.js
@@ -1,25 +1,24 @@
-require('dotenv').config();
+require('dotenv').config(); // Pastikan ini ada di paling atas
 
 module.exports = {
-    "development": {
-        "username": process.env.DB_USERNAME,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.HOST,
-        "dialect": process.env.DB_DIALECT,
-      },
-      "test": {
-        "username": process.env.DB_USERNAME,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.HOST,
-        "dialect": process.env.DB_DIALECT
-      },
-      "production": {
-        "username": process.env.DB_USERNAME,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.HOST,
-        "dialect": process.env.DB_DIALECT
-      },
-}
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL', // Gunakan nama variabel yang sesuai
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
+
+console.log(process.env.DATABASE_URL); // Untuk debugging
